680de0fd9165a41836bc9441a6605ee2ff853ff1
Abdulwahab <mjhdaais@gmail.com>
2018-01-31 01:15:06 +0100

    v1.0.3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app, r
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import SignUpForm
 
 @app.route('/')
@@ -7,7 +7,12 @@ from app.forms import SignUpForm
 def index():
 	return render_template('index.html')
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
 	form = SignUpForm()
+	if request.method == 'POST':
+		flash('Hi ' + str(form.acc_name.data) + ' you are not registered yet \
+		because the system is not yet completed. \
+		But you can check time to time to see the development is going')
+		return redirect(url_for('index'))
 	return render_template('sign_up.html', form=form)
