11dfe31174a64d508da392f067560ed5889408f9
Abdulwahab <mjhdaais@gmail.com>
2018-02-10 12:10:54 +0100

    v1.0.5

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from app.models import User
 from app.forms import SignUpForm, SignInForm
-from flask_login import login_user
+from flask_login import login_user, login_required
 
 @app.route('/')
 @app.route('/index')
@@ -38,3 +38,9 @@ def sign_in():
 		flash('Signed in successfully.')
 		return redirect(url_for('dashboard'))
 	return render_template('sign_in.html', title='Sign In', form=form)
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+	return render_template('dashboard.html', title='Dashboard')
