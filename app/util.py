0993e70de9c4f5680ba4b2155e31bb18e0aae6f1
Abdulwahab Abdullateef <mjhdaais@gmail.com>
2018-03-20 13:21:06 +0100

    v1.1.7

--- app/util.py
+++ app/util.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash
 
 
 now = datetime.utcnow()
@@ -19,3 +20,59 @@ def date_diff(then, now):
 	overall_hours = days_to_hours + diff_btw_two_times
 
 	return int(overall_hours)
+
+
+def insert_users(db, User):
+	
+	db.drop_all()
+	db.create_all()
+
+	#user_1 = User(email='mjhdaais@gmail.com',
+	#			phone='07065385493', 
+	#	        password=generate_password_hash('mjhd'),
+	#	        bank_name='First Bank', 
+	#	        acc_no='3036040428', 
+	#	        acc_name='Abdulwahab Abdullateef',
+	#	        role='crtr',
+	#	        stock=2000,
+	#	        status='seller')
+
+	user_2 = User(email='maim@gmail.com',
+				phone='07035948710',
+				password=generate_password_hash('maim'),
+		        bank_name='Ecobank', 
+		        acc_no='0306040191', 
+		        acc_name='Maimuna Abdullateef',
+		        role='intx',
+		        status='seller')
+
+	user_3 = User(email='husna@gmail.com',
+				phone='08198447511',
+				password=generate_password_hash('husn'), 
+		        bank_name='UBA', 
+		        acc_no='07145670911', 
+		        acc_name='Hussaina Abdullateef')
+
+	user_4 = User(email='illiyas@gmail.com',
+				phone='07067491101',
+				password=generate_password_hash('illi'), 
+		        bank_name='FCMB', 
+		        acc_no='0145934191', 
+		        acc_name='Illiyasu Abdullateef')
+
+	user_5 = User(email='hasna@gmail.com',
+				phone='09067011243',
+				password=generate_password_hash('hasn'), 
+		        bank_name='Zenith Bank', 
+		        acc_no='0711110124', 
+		        acc_name='Hassana Abdullateef')
+
+	user_6 = User(email='ummi@gmail.com',
+				phone='07065303011',
+				password=generate_password_hash('ummi'), 
+		        bank_name='Diamond Bank', 
+		        acc_no='1110756341', 
+		        acc_name='Fatima Abdullateef')
+
+	db.session.add_all([user_2, user_3, user_4, user_5, user_6])
+	db.session.commit()
--- maxcash.py
+++ maxcash.py
@@ -1,66 +1,13 @@
 #import os
 from app import db, create_app, celery
 from app.models import User
+from app.util import insert_users
 
 #config_name = os.getenv('FLASK_CONFIG')
 app = create_app('production')
 app.app_context().push()
 
-"""
-db.drop_all()
-
-db.create_all()
-
-user_1 = User(email='mjhdaais@gmail.com',
-			phone='07065385493', 
-	        password='mjhd',
-	        bank_name='First Bank', 
-	        acc_no='3036040428', 
-	        acc_name='Abdulwahab Abdullateef',
-	        role='crtr',
-	        stock=2000,
-	        status='seller')
-
-user_2 = User(email='maim@gmail.com',
-			phone='07035948710', 
-	        password='maim',
-	        bank_name='Ecobank', 
-	        acc_no='0306040191', 
-	        acc_name='Maimuna Abdullateef',
-	        role='intx',
-	        status='seller')
-
-user_3 = User(email='husna@gmail.com',
-			phone='08198447511', 
-	        password='husn',
-	        bank_name='UBA', 
-	        acc_no='07145670911', 
-	        acc_name='Hussaina Abdullateef')
-
-user_4 = User(email='illiyas@gmail.com',
-			phone='07067491101', 
-	        password='illi',
-	        bank_name='FCMB', 
-	        acc_no='0145934191', 
-	        acc_name='Illiyasu Abdullateef')
-
-user_5 = User(email='hasna@gmail.com',
-			phone='09067011243', 
-	        password='hasn',
-	        bank_name='Zenith Bank', 
-	        acc_no='0711110124', 
-	        acc_name='Hassana Abdullateef')
-
-user_6 = User(email='ummi@gmail.com',
-			phone='07065303011', 
-	        password='ummi',
-	        bank_name='Diamond Bank', 
-	        acc_no='1110756341', 
-	        acc_name='Fatima Abdullateef')
-
-db.session.add_all([user_1, user_2, user_3, user_4, user_5, user_6])
-db.session.commit()
-"""
+#insert_users(db, User)
 
 @app.shell_context_processor
 def make_shell_context():
